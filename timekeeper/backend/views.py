794a122553b8fd3f1d8922315af9700de2f5b451
Gabriel Trindade <gabrielsvtrindade@gmail.com>
2022-12-08 08:31:24 -0300

    First views added

--- timekeeper/backend/views.py
+++ timekeeper/backend/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(req):
+    return render(req, "backend/index.html")
+
+
+def login(req):
+    return render(req, "backend/login.html")
+
+
+def register(req):
+    return render(req, "backend/register.html")
+
+
+def history(req):
+    return render(req, "backend/history.html")
