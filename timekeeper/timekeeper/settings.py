d08e2a0c47478ceb852b33b1c691b8e35d28f626
Gabriel Trindade <gabrielsvtrindade@gmail.com>
2023-01-26 19:32:35 -0300

    New branch

--- timekeeper/timekeeper/settings.py
+++ timekeeper/timekeeper/settings.py
@@ -88,11 +88,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "/static/static"
-MEDIA_URL = "/static/media"
+STATIC_URL = "/static/static/"
+MEDIA_URL = "/static/media/"
 
-STATIC_ROOT = "/vol/web/static"
-MEDIA_ROOT = "/vol/web/media"
+STATIC_ROOT = "/vol/web/static/"
+MEDIA_ROOT = "/vol/web/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
