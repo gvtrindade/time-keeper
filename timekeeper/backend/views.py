1d2f464cc45c7df110f401b05559f7179109fca1
Gabriel Trindade <52544804+gvtrindade@users.noreply.github.com>
2023-03-30 15:40:07 -0300

    Code Refactor

--- timekeeper/backend/views.py
+++ timekeeper/backend/views.py
@@ -7,11 +7,7 @@ from .models import Record
 from auths.models import CustomUser
 from datetime import datetime, timedelta
 
-SIXTY_SECONDS = 60
-ZERO_HOUR = 0
-TWELVE_HOURS = 12
-TWENTY_FOUR_HOURS = 24
-
+CURRENT_WEEK = f"month=false&year={datetime.now().year}&number={datetime.now().strftime('%V')}"
 
 def index(request):
     return redirect("/auths")
@@ -33,11 +29,9 @@ def history(request):
         break_duration = request.POST.get("breakDuration")
 
         record = Record()
-        record.create_record(user, status, action,
-                             break_duration=break_duration)
+        record.create_record(user, status, action, break_duration=break_duration)
 
-        current_week = f"?month=false&year={datetime.now().year}&number={datetime.now().strftime('%V')}"
-        return redirect(f"/history{current_week}")
+        return redirect(f"/history?{CURRENT_WEEK}")
     else:
         user = request.user
         year = int(request.GET.get('year'))
@@ -96,14 +90,12 @@ def include(request):
         record.create_record(user, status, action, date,
                              time, time_period, break_duration, remarks)
 
-        current_week = f"?month=false&year={datetime.now().year}&number={datetime.now().strftime('%V')}"
-        return redirect(f"/history{current_week}")
+        return redirect(f"/history?{CURRENT_WEEK}")
     else:
         return render(request, "backend/include.html")
 
 
 def user_list(request):
-    current_week = f"?month=false&year={datetime.now().year}&number={datetime.now().strftime('%V')}"
     if not request.user.is_authenticated:
         messages.success(request, "You must login to access this page")
         return redirect("/auths")
@@ -114,7 +106,7 @@ def user_list(request):
 
     if not request.user.is_staff:
         messages.success(request, "You can't access this page")
-        return redirect(f"/history{current_week}")
+        return redirect(f"/history?{CURRENT_WEEK}")
 
     users = CustomUser.objects.all
     context = {"users": users}
@@ -122,7 +114,6 @@ def user_list(request):
 
 
 def user(request, user_id):
-    current_week = f"?month=false&year={datetime.now().year}&number={datetime.now().strftime('%V')}"
     if not request.user.is_authenticated:
         messages.success(request, "You must login to access this page")
         return redirect("/auths")
@@ -133,7 +124,7 @@ def user(request, user_id):
 
     if not request.user.is_staff:
         messages.success(request, "You can't access this page")
-        return redirect(f"/history{current_week}")
+        return redirect(f"/history?{CURRENT_WEEK}")
 
     listed_user = CustomUser.objects.get(id=user_id)
     record = Record()
@@ -172,7 +163,7 @@ def user(request, user_id):
         context = {
             "listed_user": listed_user,
             "records": records,
-            "workedHours": calculate_worked_hours(records),
+            "workedHours": record.calculate_worked_hours(records),
             "years": range(earliest_record, datetime.now().year + 1),
         }
     except:
@@ -192,80 +183,6 @@ def user(request, user_id):
     }
 
     return render(request, "backend/user.html", context)
-
-
-def get_date(request):
-    time = request.POST.get("time")
-    time_period = request.POST.get("timePeriod")
-    if time_period == "PM":
-        hours = ZERO_HOUR if int(
-            time[:2]) + TWELVE_HOURS == TWENTY_FOUR_HOURS else int(time[:2]) + TWELVE_HOURS
-        minutes = time[3:]
-        time = f'{hours}:{minutes}'
-    datetime_str = f'{request.POST.get("date")} {time}'
-    return datetime.strptime(datetime_str, "%Y-%m-%d %H:%M")
-
-
-def get_records(request):
-    year = request.GET.get("year") if request.GET.get(
-        "year") else datetime.now().year
-    number = (
-        request.GET.get("number")
-        if request.GET.get("number")
-        else datetime.now().strftime("%V")
-    )
-    query = Q(user=request.user, date__year=int(year))
-
-    if request.GET.get("month") == "true":
-        query.add(Q(date__month=int(number)), "AND")
-    else:
-        query.add(Q(date__week=int(number)), "AND")
-
-    return Record.objects.filter(query).order_by("date__day", "date__hour")
-
-
-def create_record(request):
-    record = Record()
-    record.user = request.user
-    record.status = "Wating Approval" if request.POST.get(
-        "date") else "Approved"
-    record.action = request.POST.get("action")
-    if request.POST.get("date"):
-        record.date = get_date(request)
-    if request.POST.get("remarks"):
-        record.remarks = request.POST.get("remarks")
-    if record.action == "Clock-out":
-        record.break_duration = request.POST.get("breakDuration")
-    record.save()
-
-
-def calculate_worked_hours(records):
-    worked_hours = timedelta(0)
-    break_duration_sum = 0
-    check_in_records = list(
-        filter(
-            lambda record: record.action == "Clock-in" and record.status == "Approved",
-            records,
-        )
-    )
-
-    for record in check_in_records:
-        record_index = next(i for i, x in enumerate(
-            list(records)) if x.id == record.id)
-
-        if record_index + 1 < len(records) and (
-                records[record_index].date.day == records[record_index + 1].date.day
-                and records[record_index + 1].action == "Clock-out"
-                and records[record_index + 1].status == "Approved"
-        ):
-            worked_hours += records[record_index +
-                                    1].date - records[record_index].date
-            break_duration_sum += records[record_index + 1].break_duration
-
-    worked_seconds = worked_hours.seconds - \
-        (break_duration_sum * SIXTY_SECONDS)
-
-    return "{:02}h{:02}".format(worked_seconds // 3600, worked_seconds % 3600 // 60)
 
 
 def delete_record(request, record_id):
