3f97588b92df577a2849d66cf3c7dfcf9ac4ad82
Gabriel Trindade <52544804+gvtrindade@users.noreply.github.com>
2023-05-05 11:09:46 -0300

    Fix settings file

--- timekeeper/timekeeper/settings.py
+++ timekeeper/timekeeper/settings.py
@@ -1,16 +1,20 @@
 import os
 from pathlib import Path
 
+import environ
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+root = environ.Path()
+env = environ.Env()
+env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 
+SECRET_KEY = env.str("SECRET_KEY", default="changeme")
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
-
-DEBUG = bool(int(os.environ.get("DEBUG", 0)))
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
-ALLOWED_HOSTS_ENV = os.environ.get("ALLOWED_HOSTS")
-if ALLOWED_HOSTS_ENV:
+ALLOWED_HOSTS_ENV = env.str("ALLOWED_HOSTS", default="")
+if len(ALLOWED_HOSTS_ENV) > 0:
     ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(","))
 
 
@@ -57,11 +61,8 @@ WSGI_APPLICATION = "timekeeper.wsgi.application"
 
 DATABASES = {
     "default": {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gabrielsvtrindad$timekeeper_db',
-        'USER': 'gabrielsvtrindad',
-        'PASSWORD': 'XznemJuYp8M29nB',
-        'HOST':'gabrielsvtrindade.mysql.pythonanywhere-services.com',
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -91,14 +92,17 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "/static/"
+STATIC_URL = "/static/static/"
+MEDIA_URL = "/static/media/"
 
-STATIC_ROOT = "/home/gabrielsvtrindade/timekeeper/backend/static/"
+STATIC_ROOT = "/vol/web/static/"
+MEDIA_ROOT = "/vol/web/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "gabrielsvtrindade@gmail.com"
-EMAIL_HOST_PASSWORD = 'cjjzlmynjzxnzenf'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+
+EMAIL_HOST = env.str("EMAIL_HOST", default="")
+EMAIL_PORT = env.int("EMAIL_PORT", default="")
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
+EMAIL_HOST_USER = env.str("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD", default="")
