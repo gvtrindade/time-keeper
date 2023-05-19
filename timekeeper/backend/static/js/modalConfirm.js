72334032ac2aadabe448caccd611ef94fa53178c
Gabriel Trindade <52544804+gvtrindade@users.noreply.github.com>
2023-05-19 10:32:19 -0300

    Show password option added; 24h format implemented; Code refactor (#6)
    * Code Refactor
    
    * Time presentation changed; Clock-out register altered; Code refactor
    
    * Add show password functionality

--- timekeeper/backend/static/js/modalConfirm.js
+++ timekeeper/backend/static/js/modalConfirm.js
@@ -2,9 +2,13 @@ csrftoken = document.getElementsByName('csrfmiddlewaretoken')[0].value
 
 let dataType, dataId;
 
-function showConfirmModal(type, title, id) {
+function showDeleteConfirmModal(type, title, id) {
+  const confirmModalTitle = document.getElementById('confirmModalTitle')
+  const modalContent = document.getElementById('delete-modal-content')
   const deletedDataType = document.getElementById('deletedDataType')
   const deletedDataTitle = document.getElementById('deletedDataTitle')
+  confirmModalTitle.innerHTML = 'Delete'
+  modalContent.hidden = false
   deletedDataType.innerHTML = type
   deletedDataTitle.innerHTML = title
   dataType = type
@@ -19,7 +23,6 @@ function handleDelete() {
     case 'record':
       deleteRecord(dataId)
       break;
-
     default:
       break;
   }
@@ -52,6 +55,42 @@ function deleteRecord(elementId) {
       'Content-Type': 'application/json',
       'X-CSRFToken': csrftoken
     }
+  })
+    .then(result => {
+      window.location.reload()
+    })
+    .catch(error => {
+      console.error(error)
+    })
+    .finally(result => {
+      dataType = ''
+      dataId = -1
+    })
+}
+
+function showRegisterConfirmModal() {
+  const confirmModalTitle = document.getElementById('confirmModalTitle')
+  const modalContent = document.getElementById('register-modal-content')
+  confirmModalTitle.innerHTML = 'Clock-out'
+  modalContent.hidden = false
+}
+
+function handleRegister() {
+  const breakDuration = document.getElementById('breakDuration').value
+  let formData = new FormData()
+  formData.append('action', 'Clock-out')
+  formData.append('breakDuration', breakDuration)
+
+  registerRecord(formData);
+}
+
+function registerRecord(formData) {
+  fetch(window.location.href, {
+    method: 'POST',
+    headers: {
+      'X-CSRFToken': csrftoken
+    },
+    body: formData
   })
     .then(result => {
       window.location.reload()
