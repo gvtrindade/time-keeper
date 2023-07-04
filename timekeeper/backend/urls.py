586f76601e43bae9b08e84bbab99904900446165
Gabriel Trindade <52544804+gvtrindade@users.noreply.github.com>
2023-07-04 09:50:10 -0300

    Add include off day; Code refactor (#13)

--- timekeeper/backend/urls.py
+++ timekeeper/backend/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("history", views.history, name="history"),
     path("include", views.include, name="include"),
+    path("offday", views.off_day, name="off_day"),
     path("users", views.user_list, name="userList"),
     path("users/<int:user_id>", views.user, name="user"),
     path("delete_record/<int:record_id>",
--- timekeeper/backend/views.py
+++ timekeeper/backend/views.py
@@ -43,7 +43,8 @@ def history(request):
 
         return redirect(f"/history?{CURRENT_WEEK}")
     else:
-        context = query_records(request)
+        user = request.user
+        context = query_records(request, user)
 
         context = {
             **context,
@@ -52,8 +53,7 @@ def history(request):
         return render(request, "backend/history.html", context)
 
 
-def query_records(request):
-    user = request.user
+def query_records(request, user):
     year = int(request.GET.get('year'))
     week = int(request.GET.get('week'))
     month = int(request.GET.get('month'))
@@ -63,7 +63,7 @@ def query_records(request):
         passed_month = month if week == -1 else None
         records = record.get_records(user, year, week, passed_month)
         earliest_record = (
-            Record.objects.filter(user=request.user).earliest(
+            Record.objects.filter(user=user).earliest(
                 "date__year").date.year
         )
         options = record.get_filter_options(year, month)
@@ -129,16 +129,39 @@ def include(request):
             record.create_record(
                 user, "Wating Approval", 'Clock-out', date, clockout_time, break_duration, remarks)
 
-        if (clockin_time):
-            record.create_record(
-                user, "Wating Approval", 'Clock-in', date, clockin_time, break_duration, remarks)
-
-        if (clockout_time):
-            record.create_record(
-                user, "Wating Approval", 'Clock-out', date, clockout_time, break_duration, remarks)
         return redirect(f"/history?{CURRENT_WEEK}")
     else:
         return render(request, "backend/include.html")
+
+def off_day(request):
+    if not request.user.is_authenticated:
+        messages.success(request, "You must login to access this page")
+        return redirect("/auths")
+    elif not request.user.is_password_reset:
+        messages.success(
+            request, "You must reset your password before accessing the application")
+        return redirect("/auths/change-password")
+
+    if not request.user.is_staff:
+        messages.success(request, "You can't access this page")
+        return redirect(f"/history?{CURRENT_WEEK}")
+    
+    if request.method == "POST":
+        user = request.POST.get("user")
+        date = request.POST.get("date")
+
+        off_day = Record()
+        off_day.create_off_day(user, date)
+
+        return redirect(f"/history?{CURRENT_WEEK}")
+    else:
+        users = CustomUser.objects.all()
+        user_names = {}
+        for user in users.iterator():
+            user_names[user.username] = f'{user.first_name} {user.last_name}'
+
+        context = {"users": user_names}
+        return render(request, "backend/offDay.html", context)
 
 
 def user_list(request):
@@ -155,7 +178,18 @@ def user_list(request):
         return redirect(f"/history?{CURRENT_WEEK}")
 
     users = CustomUser.objects.all
-    context = {"users": users}
+    earliest_record = (
+        Record.objects.earliest(
+            "date__year").date.year
+    )
+    
+    context = {
+        "users": users,
+        "years": range(earliest_record, datetime.now().year + 1),
+        "months": MONTHS,
+        "selectedYear": datetime.now().year,
+        "selectedMonth": datetime.now().month
+    }
     return render(request, "backend/userList.html", context)
 
 
@@ -196,39 +230,7 @@ def user(request, user_id):
             edited_record.status = request.POST.get("status")
             edited_record.save()
 
-    try:
-        year = int(request.GET.get('year'))
-        week = int(request.GET.get('week'))
-        month = int(request.GET.get('month'))
-        passed_month = month if week == -1 else None
-        records = record.get_records('', year, week, passed_month)
-        earliest_record = (
-            Record.objects.filter(user=listed_user).earliest(
-                "date__year").date.year
-        )
-        options = record.get_filter_options(year, month)
-
-        context = {
-            "listed_user": listed_user,
-            "records": records,
-            "workedHours": record.calculate_worked_hours(records),
-            "years": range(earliest_record, datetime.now().year + 1),
-            "selectedYear": year,
-            "months": MONTHS,
-            "selectedMonth": month,
-            "selectedWeek": week,
-            "options": options
-        }
-    except:
-        context = {
-            "records": [],
-            "years": range(datetime.now().year, datetime.now().year + 1),
-            "selectedYear": year,
-            "months": MONTHS,
-            "selectedMonth": month,
-            "selectedWeek": week,
-            "options": {}
-        }
+    context = query_records(request, listed_user)
 
     context = {
         **context,
@@ -243,6 +245,7 @@ def delete_record(request, record_id):
     if not request.user.is_authenticated or not request.user.is_staff:
         messages.success(request, "You must login to access this page")
         return redirect("/auths")
+    
     if request.method == "DELETE":
         print("Record: ", record_id)
         record = Record.objects.get(pk=record_id)
